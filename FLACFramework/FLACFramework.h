36ca3522a7f01f80f85dbafd343d29e79a2057d3
Jacob Jennings <jjennings@navdy.com>
2017-03-07 17:09:54 -0800

    WIP

---
+++ FLACFramework/FLACFramework.h
@@ -0,0 +1,19 @@
+//
+//  FLACFramework.h
+//  FLACFramework
+//
+//  Created by Jacob Jennings on 3/7/17.
+//  Copyright © 2017 UNLV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FLACFramework.
+FOUNDATION_EXPORT double FLACFrameworkVersionNumber;
+
+//! Project version string for FLACFramework.
+FOUNDATION_EXPORT const unsigned char FLACFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FLACFramework/PublicHeader.h>
+
+
---
+++ FLACiOS/FLACEncoder.swift
@@ -0,0 +1,13 @@
+//
+//  FLACEncoder.swift
+//  FLACiOS
+//
+//  Created by Jacob Jennings on 3/7/17.
+//  Copyright © 2017 UNLV. All rights reserved.
+//
+
+import Foundation
+
+class FLACEncoder {
+    // https://github.com/oneman/libflac/blob/master/examples/c/encode/file/main.c
+}
